30c378f7221658bc41f70b53e3de9938644c1ce4
funcsom <bishop@kau.kr>
2024-11-27 15:40:12 +0900

    js 주석처리하며 곱씹기, 필요없는 코드 삭제, 코드 최적화

--- kiosk_1126/kiosk.js
+++ kiosk_1126/kiosk.js
@@ -1,41 +1,91 @@
+// 단락의 가장 위 주석은 실행 시점, 아래의 코드 프리뷰를 나타냄
+// 단락의 중간 주석은 바로 위 코드 단위의 기능에 대해 서술
+
+// page structure
+
+// Category id="category"
+// | Home | Drinks | Dessert |
+//
+// Menu
+// 선택한 카테고리에 해당하는 메뉴 나열 (동적 렌더링)
+//
+// Cart
+// 메뉴 선택 시 이곳에 업데이트됨. + 버튼, - 버튼, delete 버튼 생성 (동적 렌더링)
+//
+// Total 금액 표시
+
 // name: { price: "3000", count: 1 } <- 구조
 const cart = {};
 
-// 각각의 요소를 querySelector로 가져옴
-const category = document.querySelector("#categories");
+// 각각의 요소를 JS에서 제어하기 위해 querySelector 이용
+const category = document.querySelector("#category");
 const menu = document.querySelector("#menu");
 const cartDisplay = document.querySelector("#cart");
 const totalDisplay = document.querySelector("#total");
 
 // 각각의 메뉴를 객체 형식으로 묶어서 category, name, price 를 key로 가지도록 부여
+// DB에서 해당 배열만 받아온다면, js나 html의 큰 수정 없이 코드 유지 가능
 const menuCategories = [
   { category: "Drinks", name: "Coffee", price: 3000 },
   { category: "Drinks", name: "Tea", price: 2500 },
-  { category: "Deserts", name: "Cake", price: 4500 },
+  { category: "Desserts", name: "Cake", price: 4500 },
 ];
 
-// 카테고리 탭에서 "버튼 태그"를 눌렀을 때,
+// 카테고리 탭에서 특정 카테고리 선택 시 ("버튼 태그"를 눌렀을 때) 작동
+// 선택한 카테고리의 메뉴들을 렌더링하는 함수 renderMenu() 호출
 category.addEventListener("click", (event) => {
   if (event.target.tagName === "BUTTON") {
     const categoryName = event.target.getAttribute("data-name");
-    // event 객체를 불러와 해당 태그의 attribute를 가져오는 getAttribute() 메서드를 이용.
-    // categoryName이라는 상수에 해당 attribute를 대입하고,
+    // PointerEvent 객체를 불러와 해당 요소의 attribute를 가져오는 getAttribute() 메서드 이용
+    // categoryName이라는 상수에 해당 attribute를 저장
     if (categoryName === "Home") {
-      renderMenu(); // Home 카테고리라면, 모든 menu를 렌더링할 수 있도록 비워둠
+      renderMenu(); // Home 카테고리라면, 모든 menu를 렌더링할 수 있도록 비워둠 - from-1)
     } else {
-      renderMenu(categoryName); // 해당 카테고리네임을 렌더할 수 있게 renderMenu()에 전달
+      renderMenu(categoryName); // Home이 아닌 카테고리라면, categoryName을 renderMenu()에 전달하여 해당 카테고리만을 render할 수 있도록 함
     }
   }
 });
 
-//
+// category.addEventListener()을 호출 시 실행
+// Menu 섹션에서 카테고리의 선택에 따라 어떤 메뉴를 렌더할 지 결정하는 함수
+function renderMenu(categoryName) {
+  menu.innerHTML = ""; // 메뉴 div의 자식 요소 초기화
+
+  const filteredMenu = categoryName
+    ? menuCategories.filter((item) => item.category === categoryName)
+    : menuCategories;
+  // categoryName 인수의 전달 여부에 따라 필터링된 메뉴를 렌더할 지, 필터링되지 않은 메뉴를 렌더할 지 - to-1)
+  // filter 메서드를 이용하여 콜백함수가 참이되는 조건 (즉, 전달된 categoryName과 menuCategories의 category 프로퍼티의 value값이 일치하는지를 비교)을 만족하는 item 객체만 filteredMenu에 배열 형태로 저장
+
+  filteredMenu.forEach(({ name, price }) => {
+    const menuItem = document.createElement("button");
+    menuItem.textContent = `${name} - (${price.toLocaleString()}원)`;
+    menuItem.classList.add(name); // 제거 - 이것도 바꾸어씀~~~~~~~~~~~~~~~~~ 왜 백틱하고 네임했냐 그냥 네임하지
+    menuItem.dataset.name = name;
+    menuItem.dataset.price = price; // 제거 - 여기서 문자열로 바뀌나????????????????? 나중에 Number하게..
+
+    menu.appendChild(menuItem);
+  });
+  // filteredMenu의 배열 값을 돌며, name과 price 프로퍼티를 가져와 버튼 형태로 렌더링
+  // classList.add()를 통해 메뉴 버튼의 class를 지정
+  // dataset.name과 dataset.price를 통해 해당 버튼의 속성을 지정
+  // 만든 메뉴버튼을 차례로 menu html요소의 자식으로 렌더링
+
+  // 제거 - !!!!!!! dataset은 커스텀 속성 "date-어쩌고" 를 js에서 지정해주는 코드이구나!~!!!!!
+
+  // 제거 - updateCart();
+  // 제거 - updateCart() 함수를 호출 <- 결국 한번의 카테고리를 누를 때마다 <- 에.. 이 코드가 필요한가? 필요없다!! 이따가 한번 점검할 때 지우자~
+}
+
+// Menu 섹션에서 메뉴 선택 시 (특정 메뉴 버튼 클릭) 작동
+// 특정 메뉴 클릭 시, 해당 메뉴를 카트에 렌더링하기 위해서 updateCart() 함수 호출
 menu.addEventListener("click", (event) => {
-  const target = event.target;
+  const target = event.target; // 제거 - 흠... name이나 price가 있는데 굳이 class를 명시해야할까? ~~~~~~~~~~~~~₩
   const name = target.dataset.name;
   const price = target.dataset.price;
-  console.log(name);
 
-  if (!name) return;
+  // 제거 - if (!name) return;
+  // 이거 지움~~~~~~~~~~~~~~~~~
 
   if (target.classList) {
     if (cart[name]) {
@@ -44,16 +94,16 @@ menu.addEventListener("click", (event) => {
       cart[name] = { price: Number(price), count: 1 };
     }
   }
-  console.log(cart);
-  updateCart();
+  // cart 객체 프로퍼티를 수정 (기존에 cart 객체 안에 일치하는 name 프로퍼티가 있으면 count를 1 올리고, 없으면 전달받은 name을 키로 갖는 프로퍼티 생성)
+
+  updateCart(); // cart 객체를 다시 불러와 새로 렌더링
 });
 
-//
+// Cart 섹션에서 cart에 담긴 메뉴 내 +, -, delete 버튼 클릭 시 작동
+// +, -, delete 버튼 클릭 시, 변경된 메뉴 개수를 Cart 섹션에 렌더링하기 위해서 updateCart() 함수 호출
 cartDisplay.addEventListener("click", (event) => {
   const target = event.target;
   const name = target.dataset.name;
-
-  if (!name) return;
 
   if (target.classList.contains("increase")) {
     cart[name].count++;
@@ -65,71 +115,59 @@ cartDisplay.addEventListener("click", (event) => {
   } else if (target.classList.contains("delete")) {
     delete cart[name];
   }
+  // 버튼의 클래스에 따라 cart 객체를 update
 
-  updateCart();
+  updateCart(); // cart 객체를 다시 불러와 새로 렌더링
 });
 
-// Menu를 카테고리에 따라 렌더하기 위해
-function renderMenu(categoryName) {
-  menu.innerHTML = "";
-
-  const filteredMenu = categoryName
-    ? menuCategories.filter((item) => item.category === categoryName)
-    : menuCategories;
-
-  console.log(filteredMenu);
-  filteredMenu.forEach(({ name, price }) => {
-    const menuItem = document.createElement("button");
-    menuItem.textContent = `${name} - (${price.toLocaleString()}원)`;
-    menuItem.classList.add(`${name}`);
-    menuItem.dataset.name = name;
-    menuItem.dataset.price = price;
-
-    menu.appendChild(menuItem);
-  });
-  console.log(cart);
-  updateCart();
-}
-
 // Cart의 UI를 업데이트하는 함수
+// cart 객체를 불러와 format에 맞게 렌더링
+// 객체를 하나하나 불러와 DOM 요소로 반환하고, 그 사이사이에 +, -, delete 버튼을 추가 (버튼에 class를 지정하여 클릭 시에 반응하도록 함.)
 function updateCart() {
   cartDisplay.innerHTML = "";
   let total = 0;
 
   for (const name in cart) {
     const { price, count } = cart[name];
+    // cart[name]을 통해 value 객체를 불러와 구조분해할당으로 각각 price, count에 할당
+
     total += price * count;
+    // total에 누적으로 더함
 
     const item = document.createElement("div");
     item.textContent = `${name} x ${count} (${(
       price * count
     ).toLocaleString()}원)`;
+    // 해당 menu에 대해서 총 가격 제시
 
-    const plusBtn = document.createElement("button");
-    plusBtn.textContent = "+";
-    plusBtn.classList.add("increase");
-    plusBtn.dataset.name = name;
+    const increaseBtn = document.createElement("button");
+    increaseBtn.textContent = "+";
+    increaseBtn.classList.add("increase");
+    increaseBtn.dataset.name = name;
 
-    const minusBtn = document.createElement("button");
-    minusBtn.textContent = "-";
-    minusBtn.classList.add("decrease");
-    minusBtn.dataset.name = name;
+    const decreaseBtn = document.createElement("button");
+    decreaseBtn.textContent = "-";
+    decreaseBtn.classList.add("decrease");
+    decreaseBtn.dataset.name = name;
 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "delete";
     deleteBtn.classList.add("delete");
     deleteBtn.dataset.name = name;
+    // 하나의 menu 항목에 3가지의 버튼 생성
 
-    item.prepend(minusBtn);
+    item.append(minusBtn);
     item.append(plusBtn);
     item.append(deleteBtn);
 
     cartDisplay.appendChild(item);
+    // item 요소에 button을 더하고, item을 cartDisplay 요소의 자식으로 추가
   }
 
   totalDisplay.textContent = total.toLocaleString();
+  // totalDisplay span에 총 가격 렌더링
 }
 
-// 1. 각각의 항목에 +, - 버튼을 붙일 수 있도록 해보기
-// 2. delete 버튼 생성하기
-// 3. tab으로 항목 구분하기
+// 전체적인 구조는, 누르는 곳에서 함수를 호출하면 되는듯?
+// 함수에서는 요소를 렌더링하고
+// 클릭이벤트에서는 렌더링된 요소의 class로 구별해서 데이터를 변경
